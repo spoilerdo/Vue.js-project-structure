cfbe0a7aa3faa6a40a0bc712a439e5791754849f
Martijn <martijn.dormans@gmail.com>
2019-09-11 15:36:44 +0200

    init

---
+++ src/views/login/logic.vue
@@ -0,0 +1,8 @@
+<template></template>
+<script>
+export default {
+    mounted() {
+        console.log("logic file WORKS!!");
+    }
+}
+</script>
---
+++ src/views/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+    <h1>LOGIN PLEASE!!!</h1>
+    <logic/>
+    </div>
+</template>
+
+<script>
+import logic from "./logic.vue";
+
+export default {
+    components: {
+        logic
+    }
+}
+</script>
